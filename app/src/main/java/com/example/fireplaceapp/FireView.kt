57e6e939613b7e6af6581fb724e757fd8682ee9a
OlegYariga <oyariga@usetech.ru>
2022-12-15 18:30:02 +0300

    feat: main activity
    - all fireplace colors

--- app/src/main/java/com/example/fireplaceapp/FireView.kt
+++ app/src/main/java/com/example/fireplaceapp/FireView.kt
@@ -21,7 +21,7 @@ class FireView : View {
     private lateinit var mSourceRect: Rect
     private lateinit var mDestRect: Rect
     private var mLastDrawTime: Long = 0
-    private val mFireColors = intArrayOf(
+    private val mFireColorsRed = intArrayOf(
         0xFFFFFFFF.toInt(),
         0xFFEFEFC7.toInt(),
         0xFFDFDF9F.toInt(),
@@ -56,6 +56,81 @@ class FireView : View {
         0xFF1f0707.toInt(),
         0
     )
+
+    private val mFireColorsGreen = intArrayOf(
+        0xFFFFFFFF.toInt(),
+        0xFFEFEFC7.toInt(),
+        0xFFDFDF9F.toInt(),
+        0xFFCFCF6F.toInt(),
+        0xFFB7B737.toInt(),
+        0xFFB7B72F.toInt(),
+        0xFFB7AF2F.toInt(),
+        0xFFbfaf2f.toInt(),
+        0xFFbfa727.toInt(),
+        0xFFbf9f1f.toInt(),
+        0xFF93c47d.toInt(),
+        0xFF84d75e.toInt(),
+        0xFF6fd442.toInt(),
+        0xFF5ec92e.toInt(),
+        0xFF57e717.toInt(),
+        0xFF37aa04.toInt(),
+        0xFF38761d.toInt(),
+        0xFF2f7411.toInt(),
+        0xFF225c09.toInt(),
+        0xFF174603.toInt(),
+        0xFF1c3c0b.toInt(),
+        0xFF20490a.toInt(),
+        0xFF48bd0b.toInt(),
+        0xFF4e962e.toInt(),
+        0xFF4e962e.toInt(),
+        0xFF3d811f.toInt(),
+        0xFF2d6b13.toInt(),
+        0xFF1b4b06.toInt(),
+        0xFF0b2002.toInt(),
+        0xFF2e9103.toInt(),
+        0xFF51f808.toInt(),
+        0xFF45818e.toInt(),
+        0
+    )
+
+    private val mFireColorsBlue = intArrayOf(
+        0xFFFFFFFF.toInt(),
+        0xFFEFEFC7.toInt(),
+        0xFFcfe2f3.toInt(),
+        0xFFadc9e2.toInt(),
+        0xFF87abcc.toInt(),
+        0xFF6499c9.toInt(),
+        0xFF4782b7.toInt(),
+        0xFF3e87c9.toInt(),
+        0xFF3076b4.toInt(),
+        0xFF205e96.toInt(),
+        0xFF124c81.toInt(),
+        0xFF30608a.toInt(),
+        0xFF26537c.toInt(),
+        0xFF1b4871.toInt(),
+        0xFF1b7bd2.toInt(),
+        0xFF1271c7.toInt(),
+        0xFF0c65b6.toInt(),
+        0xFF075aa4.toInt(),
+        0xFF064f91.toInt(),
+        0xFF043a6b.toInt(),
+        0xFF0d839c.toInt(),
+        0xFF106c7f.toInt(),
+        0xFF065c6e.toInt(),
+        0xFF034553.toInt(),
+        0xFF03abcf.toInt(),
+        0xFF037187.toInt(),
+        0xFF084a86.toInt(),
+        0xFF02294c.toInt(),
+        0xFF021b31.toInt(),
+        0xFF047eed.toInt(),
+        0xFF047eed.toInt(),
+        0xFF002343.toInt(),
+        0
+    )
+
+    private var mFireColors = mFireColorsRed
+
     private val mHandler = Handler(Looper.getMainLooper(), Handler.Callback {
         invalidate()
         spreadFire()
@@ -124,6 +199,17 @@ class FireView : View {
         }
         mHandler.removeCallbacksAndMessages(null)
         mHandler.sendEmptyMessage(0)
+    }
+
+    fun changeColour(i: Int){
+        this.init()
+        if (i==0){
+            this.mFireColors = mFireColorsRed
+        }else if (i == 1) {
+            this.mFireColors = mFireColorsBlue
+        }else if (i == 2) {
+            this.mFireColors = mFireColorsGreen
+        }
     }
 
     fun stopFire() {
--- app/src/main/java/com/example/fireplaceapp/MainActivity.kt
+++ app/src/main/java/com/example/fireplaceapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.fireplaceapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageButton
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,6 +20,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         start_button.setOnClickListener(this)
         stop_button.setOnClickListener(this)
+
+        radioGroup.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener {group, checkedId ->
+                if (checkedId == 2131296744) fire_view.changeColour(0)
+                if (checkedId == 2131296745) fire_view.changeColour(1)
+                if (checkedId == 2131296746) fire_view.changeColour(2)
+            })
+
     }
 
     override fun onClick(v: View?) {

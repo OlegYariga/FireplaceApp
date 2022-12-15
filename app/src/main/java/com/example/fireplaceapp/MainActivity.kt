26ca23b653adb1b7a0545b8685479410e0134674
OlegYariga <oyariga@usetech.ru>
2022-12-15 13:10:29 +0300

    feat: start making fireplace application
    - add 2 activities
    - shitching between activities
    - settings flow (base)

---
+++ app/src/main/java/com/example/fireplaceapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.fireplaceapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<ImageButton>(R.id.imageButton)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.mainmenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
